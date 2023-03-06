65dc65d937341562705b9c789a6463e3624ed75c
davidlealo <davidlealo@gmail.com>
2023-03-06 20:07:56 -0300

    Rutas de perfil, productos y usuarios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
@@ -8,4 +9,14 @@ app.listen(3000, () =>{
 
 app.get('/perfil', (req, res) =>{
     res.send('David Leal Olivares')
+})
+
+app.get('/productos', (req, res) => {
+    const productos = JSON.parse(fs.readFileSync('productos.json'))
+    res.json(productos)
+})
+
+app.get('/usuarios', (req, res) =>{
+    const usuarios = JSON.parse(fs.readFileSync('usuarios.json'))
+    res.json(usuarios)
 })
